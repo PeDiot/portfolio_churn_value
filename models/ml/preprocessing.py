44b94611f52276e1ce5ec3cb94299c42aeef482d
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2022-01-21 13:18:10 +0100

    Remove variables in preprocessing.

--- models/ml/preprocessing.py
+++ models/ml/preprocessing.py
@@ -36,20 +36,20 @@ In [10]: with open(file_path, "rb") as file:
 
 In [11]: train
 Out[11]: 
-CleanedData(X=       Latitude   Longitude  Gender Senior_Citizen Partner Dependents  ... Streaming_Movies        Contract Paperless_Billing    Payment_Method Monthly_Charges Total_Charges      
-0     36.527243 -118.594938    Male             No      No         No  ...               No  Month-to-month               Yes  Electronic check           24.25         24.25
-1     33.936291 -118.332639    Male             No      No         No  ...               No  Month-to-month               Yes  Electronic check           89.95       1178.40
-2     36.641520 -121.622188  Female             No     Yes        Yes  ...              Yes        Two year               Yes     Bank transfer          115.80       8476.50
-3     37.990574 -120.261821    Male             No     Yes         No  ...               No  Month-to-month               Yes     Bank transfer           79.35        661.25
-4     34.066964 -117.937007    Male             No     Yes        Yes  ...              Yes        One year                No       Credit card          105.75       7322.50
-...         ...         ...     ...            ...     ...        ...  ...              ...             ...               ...               ...             ...           ...
-4917  37.801776 -122.402293  Female             No     Yes        Yes  ...               No        Two year               Yes     Bank transfer           25.00        332.50
-4918  33.000269 -117.072093    Male             No      No        Yes  ...               No  Month-to-month                No       Credit card           25.00        789.20
-4919  39.117537 -122.284654    Male            Yes      No         No  ...               No  Month-to-month                No      Mailed check           73.75        545.15
-4920  38.232389 -122.324944    Male            Yes      No         No  ...               No  Month-to-month               Yes  Electronic check           59.90        788.35
-4921  36.798882 -120.019511    Male             No      No         No  ...              Yes  Month-to-month               Yes  Electronic check           99.95       1931.75
+CleanedData(X=      Gender Senior_Citizen Partner Dependents  Tenure_Months  ...        Contract Paperless_Billing    Payment_Method Monthly_Charges Total_Charges
+0       Male             No      No         No            1.0  ...  Month-to-month               Yes  Electronic check           24.25         24.25
+1       Male             No      No         No           14.0  ...  Month-to-month               Yes  Electronic check           89.95       1178.40
+2     Female             No     Yes        Yes           72.0  ...        Two year               Yes     Bank transfer          115.80       8476.50
+3       Male             No     Yes         No            9.0  ...  Month-to-month               Yes     Bank transfer           79.35        661.25
+4       Male             No     Yes        Yes           68.0  ...        One year                No       Credit card          105.75       7322.50
+...      ...            ...     ...        ...            ...  ...             ...               ...               ...             ...           ...
+4917  Female             No     Yes        Yes           13.0  ...        Two year               Yes     Bank transfer           25.00        332.50
+4918    Male             No      No        Yes           31.0  ...  Month-to-month                No       Credit card           25.00        789.20
+4919    Male            Yes      No         No            7.0  ...  Month-to-month                No      Mailed check           73.75        545.15
+4920    Male            Yes      No         No           13.0  ...  Month-to-month               Yes  Electronic check           59.90        788.35
+4921    Male             No      No         No           19.0  ...  Month-to-month               Yes  Electronic check           99.95       1931.75
 
-[4922 rows x 20 columns], y=array([2846., 5463., 5841., ..., 5638., 5254., 3670.]), numeric_features=['Latitude', 'Longitude', 'Monthly_Charges', 'Total_Charges'], categorical_features=['Gender', 'Senior_Citizen', 'Partner', 'Dependents', 'Phone_Service', 'Multiple_Lines', 'Internet_Service', 'Online_Security', 'Online_Backup', 'Device_Protection', 'Tech_Support', 'Streaming_TV', 'Streaming_Movies', 'Contract', 'Paperless_Billing', 'Payment_Method'])
+[4922 rows x 19 columns], y=array([2846., 5463., 5841., ..., 5638., 5254., 3670.]), numeric_features=['Tenure_Months', 'Monthly_Charges', 'Total_Charges'], categorical_features=['Gender', 'Senior_Citizen', 'Partner', 'Dependents', 'Phone_Service', 'Multiple_Lines', 'Internet_Service', 'Online_Security', 'Online_Backup', 'Device_Protection', 'Tech_Support', 'Streaming_TV', 'Streaming_Movies', 'Contract', 'Paperless_Billing', 'Payment_Method'])
 """
 
 from os import stat
@@ -71,7 +71,7 @@ from typing import (
 import sys
 sys.path.append('../')
 
-BACKUP_PATH = "./backup/ml/"
+BACKUP_PATH = "C:/Users/pemma/OneDrive - Université de Tours/Mécen/M2/S2/04 - 3R/portfolio_churn_value/backup/ml/"
 DATA_PATH = "C:/Users/pemma/OneDrive - Université de Tours/Mécen/M2/S2/04 - 3R/portfolio_churn_value/data/"
 
 def load_rdata(file_name: str) -> Dict[str, DataFrame]: 
@@ -116,8 +116,9 @@ class Preprocessing:
             labels=[
                 "CustomerID",
                 "City", 
+                "Latitude", 
+                "Longitude", 
                 "Zip_Code",
-                "Tenure_Months",  
                 "Churn_Label", 
                 "Churn_Value", 
                 "Churn_Score", 
