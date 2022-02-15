1e2ee725b30f0138e30493f7d309dfba5e54ae7b
Pierre-Emmanuel Diot <p.emmanuel.diot@gmail.com>
2022-02-15 18:30:24 +0100

    Process to compute portfolio vraw value.

--- models/portfolio_value.R
+++ models/portfolio_value.R
@@ -6,7 +6,7 @@
 
   # For each period (T=72): 
     # Retrieve the estimated survival probability 
-    # Calculate the updated CLV using the discount factor 
+    # Calculate the updated monthly price using the discount factor 
   # Sum over the N customers who are in the portfolio 
 
 # 2. Using clustering results
@@ -14,12 +14,15 @@
   # For each K clusters: 
     # Retrieve the most representative individual 
     # Compute her estimated survival 
-    # Calculate her updated CLV using the discount factor 
+    # Calculate her updated monthly price using the discount factor 
   # Sum over the K clusters 
 
 # Note: give confidence interval for the expected portfolio value 
 
-# ----- Setup 
+# Change in portfolio value after a change in some variables 
+  # ex: what portfolio value with 70% month-to-month contract customers?
+
+# ----- Setup -----
 
 dir <- "./models/"
 setwd(dir)
@@ -39,53 +42,60 @@ library(latex2exp)
 
 theme_set(theme_minimal())
 
-# ----- Requirements 
-
-surv_fit <- survfit(cox_final, 
-                    newdata = cleaned_data)   # survfit object to estimate survival
+# ----- Requirements -----
 
 custIDs <- cleaned_data %>%
-  pull(CustomerID) %>%
-  as.character()                              # customer unique IDs
+  pull(CustomerID)                            # customer unique IDs
 
 a <- .08                                      # discount factor 
 
+num_months <- 1:72
 
-# ----- Functions
+surv_fit <- survfit(final_cox, 
+                    newdata = cleaned_data)   # survfit object to estimate survival
+
+surv <- surv_fit$surv %>%
+  as.data.frame()                             # estimated survival probabilities
+colnames(surv) <- custIDs
+
+surv_lower <- surv_fit$lower %>%
+  as.data.frame()                             # estimated lower bounds
+colnames(surv_lower) <- custIDs
+
+surv_upper <- surv_fit$upper %>%
+  as.data.frame()                             # estimated upper bounds
+colnames(surv_upper) <- custIDs
+
+# ----- Functions -----
 
 estimate_survival <- function(custID){
   
   # Return number of months, conditional survival and confidence interval for a given client.
   
-  surv <- surv_fit$surv %>%
-    as.data.frame() %>%
-    dplyr::select(all_of(custID))  
-  num_months <- as.numeric(rownames(surv))
-  surv_lower <- surv_fit$lower %>%
-    as.data.frame() %>%
+  s <- surv %>%
+    dplyr::select(all_of(custID)) %>%
+    dplyr::pull(custID)
+  l <- surv_lower %>%
+    dplyr::select(all_of(custID)) %>%
     dplyr::pull(custID) 
-  surv_upper <- surv_fit$upper %>%
-    as.data.frame() %>%
-    dplyr::pull(custID) 
-  surv <- surv %>%
-    dplyr::pull(custID) 
+  u <- surv_upper %>%
+    dplyr::select(all_of(custID)) %>%
+    dplyr::pull(custID)  
   
-  data.frame(
-    num_months = num_months, 
-    surv_lower = ifelse(is.na(surv_lower), 0, surv_lower), 
-    surv = surv, 
-    surv_upper = ifelse(is.na(surv_upper), 0, surv_upper)
-  )
+  data.frame(num_months = num_months, 
+             surv_lower = ifelse(is.na(l), 0, l), 
+             surv = s, 
+             surv_upper = ifelse(is.na(u), 0, u))
 }
 
-compute_cust_total_value <- function(surv_dat, clv){
+compute_cust_total_value <- function(surv_dat, price){
   
   # Return customer value over all periods and IC bounds.
   
   surv_dat %>%
-    dplyr::mutate( v_lower = clv * surv_lower / (1 + a)^num_months, 
-            v = clv * surv / (1 + a)^num_months,
-            v_upper = clv * surv_upper / (1 + a)^num_months ) %>%
+    dplyr::mutate( v_lower = price * surv_lower / (1 + a)^num_months, 
+            v = price * surv / (1 + a)^num_months,
+            v_upper = price * surv_upper / (1 + a)^num_months ) %>%
     dplyr::select(starts_with("v")) %>%
     colSums()
   
@@ -96,13 +106,13 @@ process <- function(custID){
   # Estimate customer survival to then calculate total value and IC bounds.
   
   surv_dat <- estimate_survival(custID) 
-  clv <- cleaned_data[as.numeric(custID), ]%>%
-    dplyr::pull(CLTV) 
-  compute_cust_total_value(surv_dat, clv)
+  price <- cleaned_data[cleaned_data$CustomerID == custID, ] %>%
+    dplyr::pull(Monthly_Charges) 
+  compute_cust_total_value(surv_dat, price)
   
 }
 
-# ----- Apply process to all customers using parallelization 
+# ----- Apply process to all customers using parallelization -----
 
 
 # NOT RUN {
@@ -115,9 +125,12 @@ process <- function(custID){
                                "compute_cust_total_value", 
                                "process", 
                                "cleaned_data",
-                               "surv_fit", 
+                               "surv",
+                               "surv_lower", 
+                               "surv_upper",
                                "custIDs", 
                                "a",
+                               "num_months",  
                                "%>%"), 
                 envir = environment())
   
@@ -131,17 +144,17 @@ process <- function(custID){
   
   custValues <- do.call(rbind, results) %>%
     as.data.frame() %>%
-    rownames_to_column(var = "CustomerID")
+    mutate(CustomerID = custIDs)
   
   save(custValues, file = paste(backup, "custValues.RData", sep = "/"))
   
 # } 
+
+# ----- Results -----
   
 load(file = paste(backup, "custValues.RData", sep = "/"))
 
 custValues %>%
-  mutate(CLTV = cleaned_data$CLTV, 
-         diff = (v - cleaned_data$CLTV) / cleaned_data$CLTV) %>%
   View()
 
 portfolio_value <- custValues %>%
@@ -163,8 +176,8 @@ custValues %>%
   geom_density(size = 1, 
                color = "blue") +
   scale_x_continuous(labels = scales::comma) +
-  labs(x = "", 
+  labs(x = "V", 
        y = "Density", 
-       title = "Distribution of customer overall value", 
+       title = "Distribution of customer raw value", 
        subtitle = subtitle) 
 
