a1e0e6f791f2aefbd32d0adc8d9ad63f58ea06cc
Pierre-Emmanuel <p.emmanuel.diot@gmail.com>
2022-02-25 11:51:42 +0100

    Update.

--- colors.R
+++ colors.R
@@ -1,4 +1,6 @@
 # ----------- Colors for Data Vizualisation ----------
 
-risk_col <- "#98A3BD"
+risk_col <- "#DD8877"
 surv_col <- "#89BCAB"
+portfol_col <- "#7B8CA8"
+monthly_charges_col <- "#75B993"
